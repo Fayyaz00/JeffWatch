dafb16ca446d7692114be5b6cf9c3f3a42a76240
ohethan <ohethan@live.unc.edu>
2020-11-09 23:17:34 -0500

    http to https

--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const login = async credentials => {
-  const baseUrl = 'http://jeffwatch.herokuapp.com/api/login'
+  const baseUrl = 'https://jeffwatch.herokuapp.com/api/login'
   const result = await axios.post(baseUrl, credentials)
   return result.data
 }
