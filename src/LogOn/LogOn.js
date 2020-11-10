4d0190f7de4579310b399e5e3741c7cd6d784d7a
Fayyaz00 <fayyazsulman@gmail.com>
2020-11-09 19:06:10 -0500

    LogOn Shell Page

---
+++ src/LogOn/LogOn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from '@reach/router';
+
+function LogOn() {
+
+    return (
+        <div className="Log-On">
+
+
+
+        </div>
+    )
+
+}
+
+export default LogOn
