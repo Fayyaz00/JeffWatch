3a85327a212b0552cdfaabd309587773f6f37c98
ohethan <ohethan@live.unc.edu>
2020-11-09 23:50:04 -0500

    Add ratings service

---
+++ src/services/ratings.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+const baseUrl = 'https://jeffwatch.herokuapp.com/api/ratings'
+
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const createRating = async (movie, rating) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const bodyParams = {
+    movie,
+    rating,
+  }
+
+  return await axios.post(baseUrl, bodyParams, config)
+}
+
+const getRating = async (movie) => {
+  const config = {
+    headers: { Authorization: token },
+    data: {
+      movie,
+    },
+  }
+
+  return await axios.get(baseUrl, config)
+}
+
+const updateRating = async (movie, rating) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const bodyParams = {
+    movie,
+    rating,
+  }
+
+  return await axios.put(baseUrl, bodyParams, config)
+}
+
+const deleteRating = async (movie) => {
+  const config = {
+    headers: { Authorization: token },
+    data: {
+      movie,
+    },
+  }
+
+  return await axios.delete(baseUrl, config)
+}
+
+export default { createRating, getRating, updateRating, deleteRating, setToken }
