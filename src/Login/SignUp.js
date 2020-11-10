444e5b9f5048e03c908da675fbb8dac461a4c99e
ohethan <ohethan@live.unc.edu>
2020-11-09 20:25:46 -0500

    Change formatting

--- src/Login/SignUp.js
+++ src/Login/SignUp.js
@@ -3,49 +3,47 @@ import { Link } from '@reach/router';
 
 function SignUp() {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(`Username: ${username}`);
-        console.log(`Password: ${password}`);
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+  }
 
-    }
-
-    return (
-        <div className="SignUp">
-            <h1>Sign-Up</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="username-form">
-                    <label>
-                        Username:
-                    <input type="text" name="username" onChange={
-                            (event) => {
-                                setUsername(event.target.value)
-                            }
-                        } minLength="4" required />
-                    </label>
-
-                </div>
-
-                <br></br>
-
-                <div className="password-form">
-                    <label>
-                        Password:
-                    <input type="password" name="password" onChange={
-                            (event) => {
-                                setPassword(event.target.value)
-                            }
-                        } minLength="4" required />
-                    </label>
-                </div>
-                <input type="submit" value="submit" />
-
-            </form>
+  return (
+    <div className="SignUp">
+      <h1>Sign-Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="username-form">
+          <label>
+            Username:
+            <input 
+              type="text" 
+              name="username" 
+              onChange={(e) => setUsername(e.target.value)}
+              minLength="4" 
+              required 
+            />
+          </label>
         </div>
-    )
+        <div className="password-form">
+          <label>
+            Password:
+            <input 
+              type="password" 
+              name="password" 
+              onChange={(e) => setPassword(e.target.value)} 
+              minLength="4" 
+              required 
+            />
+          </label>
+        </div>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
 }
 
 export default SignUp
